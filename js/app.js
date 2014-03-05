abfbfa312fcc7b94f7ddb93c4855417624d65392
Ajay Vallabh <ajay.vallabh@gmail.com>
2014-03-05 17:45:01 -0500

    Updated ember-data.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 App = Ember.Application.create();
+
+App.Router.map(function() {
+  // routes
+});
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return ['foo', 'bar']
+  }
+});
