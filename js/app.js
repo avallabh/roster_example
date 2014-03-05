0df37f09f2b4512f7738d8b79111ff1867c94e10
Ajay Vallabh <ajay.vallabh@gmail.com>
2014-03-05 18:32:10 -0500

    Added home controller with info model

--- js/app.js
+++ js/app.js
@@ -1,11 +1,25 @@
-App = Ember.Application.create();
+if (typeof PC == "undefined" || !PC) {
+  var PC = {};
+}
 
-App.Router.map(function() {
-  // routes
+PC.League = Ember.Application.create({
+  LOG_TRANSITIONS: true
 });
 
-App.IndexRoute = Ember.Route.extend({
+PC.League.Router.map(function() {
+  this.route("home", { path: "/" });
+  this.resource('team', { path: '/team/:team_id' });
+});
+
+PC.League.HomeRoute = Ember.Route.extend({
   model: function() {
-    return ['foo', 'bar']
+    return ['Coed', 'Saturdays 9am-3pm', 'at Park Center'];
+  },
+  setupController: function(controller, model) {
+    controller.set('info', model);
   }
 });
+
+// PC.BballLeague.HomeController = Ember.Controller.extend({
+//       info: ['Coed', 'Saturdays 9am-3pm', 'at Park Center'];
+//  });
